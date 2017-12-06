e3f9f222eac5a09c8bdbbe360a78e4af52a23890
Tom Barron <tusculum@gmail.com>
2017-12-05 19:10:12 -0500

    flask_app.py: Populate extra_files list and pass it to app.run()
    Routine generate_file_list() generates the list. When flask_app.py is run
    from the command line (and therefore __name__ == '__main__'), we pass the
    list from generate_file_list() as the extra_files argument to flask's
    app.run().

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import glob
 
 app = Flask(__name__)
 
@@ -50,3 +51,22 @@ def tests(payload):
     return render_template('tests/jasmine.html', payload=payload)
 
 
+# -----------------------------------------------------------------------------
+def generate_file_list():
+    glist = ['templates/*.html',
+             'templates/tests/*.html',
+             'static/js/*.js',
+             'static/js/tests-jasmine/*.js',
+             ]
+    rval = []
+    for gexpr in glist:
+        tmp = glob.glob(gexpr)
+        rval.extend(tmp)
+    return rval
+
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    flist = generate_file_list()
+    print(flist)
+    app.run(extra_files=flist)
