3d0efc0c6edb8c8eb81a886632432e2b93c1d00b
Tom Barron <tusculum@gmail.com>
2017-12-05 19:14:54 -0500

    flask_app.py: Add dividers

--- flask_app.py
+++ flask_app.py
@@ -7,38 +7,45 @@ import glob
 app = Flask(__name__)
 
 
+# -----------------------------------------------------------------------------
 @app.before_request
 def before_request():
     if 'localhost' in request.host_url:
         app.jinja_env.cache = {}
 
 
+# -----------------------------------------------------------------------------
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
 
+# -----------------------------------------------------------------------------
 @app.route('/cal-p-lb')
 def calories_per_pound():
     return render_template('calorease.html')
 
 
+# -----------------------------------------------------------------------------
 @app.route('/compounding')
 def compounding():
     return "This computes interest compounding over time"
 
 
+# -----------------------------------------------------------------------------
 @app.route('/mental_hygiene')
 def mental_hygiene():
     return render_template('mental_hygiene.html',
                            title="Mental Hygiene")
 
 
+# -----------------------------------------------------------------------------
 @app.route('/wandro')
 def wandro():
     return render_template('wandro.html')
 
 
+# -----------------------------------------------------------------------------
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html',
