226e0a54917ef47f38e2ba1958be1c94d2a20790
Tom Barron <tusculum@gmail.com>
2017-12-05 22:10:29 -0500

    calendar-spec.js: Test for rd_by_year()

--- static/js/tests-jasmine/calendar-spec.js
+++ static/js/tests-jasmine/calendar-spec.js
@@ -24,4 +24,21 @@ describe("calendar", function() {
         expect(result).toEqual("<tr><td>2017<td>tue</tr>")
     });
 
+    it("rd_by_year", function() {
+        var now = new Date();
+        var start_year = now.getYear() + 1900 - 5;
+        var rwdl = rotate(weekday_list(), 1);
+        var rdtab = rd_by_year();
+        var years = rdtab.match(/<td>(\d{4})<td>/g);
+        var wdays = rdtab.match(/<td>(...)<\/tr>/g);
+        expect(years.length).toEqual(wdays.length);
+        for (idx = 0 ; idx < 20 ; idx++) {
+            exp_yr = start_year + idx;
+            yr = years[idx].replace(/<td>/g, "");
+            wd = wdays[idx].replace(/<td>/, "").replace(/<\/tr>/, "");
+            expect(yr).toEqual("" + exp_yr);
+            apr04 = new Date(start_year+idx, 3, 4, 0, 0, 0, 0);
+            expect(wd).toEqual(rwdl[apr04.getDay()]);
+        }
+    });
 });
