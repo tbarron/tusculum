e99faaaabae461f69eebc9a8a873b206cf376984
Tom Barron <tusculum@gmail.com>
2017-12-31 18:25:14 -0500

    Add CCS, HTML, and python code for useful

---
+++ useful.py
@@ -0,0 +1,67 @@
+import py
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+import sqlalchemy as sql
+
+Base = declarative_base()
+
+# -----------------------------------------------------------------------------
+class Thought(Base):
+    __tablename__ = 'thoughts'
+
+    th_id = sql.Column(sql.Integer, primary_key=True)
+    th_vetted = sql.Column(sql.Integer)
+    th_text = sql.Column(sql.String)
+
+    def __repr__(self):
+        msg = "<Thought(th_id='{}', th_vetted='{}', th_text='{}')>"
+        return msg.format(self.th_id, self.th_vetted, self.th_text)
+            
+
+# -----------------------------------------------------------------------------
+def engine():
+    if not hasattr(engine, "_eng"):
+        dbname = py.path.local("useful.db")
+        dbselector = "sqlite:///{}".format(dbname.strpath)
+        engine._eng = sql.create_engine(dbselector)
+    return engine._eng
+
+
+# -----------------------------------------------------------------------------
+def session():
+    if not hasattr(session, "factory"):
+        eng = engine()
+        session.factory = sessionmaker(bind=eng)
+    return session.factory()
+
+
+# -----------------------------------------------------------------------------
+def setup_database():
+    """
+    Define the database of useful thoughts
+    """
+    eng = engine()
+    Thought.metadata.create_all(eng)
+
+
+# -----------------------------------------------------------------------------
+def store_thought(text):
+    """
+    Define the database of useful thoughts
+    """
+    S = session()
+    S.add(Thought(th_vetted=0, th_text=text))
+    S.commit()
+
+
+# -----------------------------------------------------------------------------
+def random_thoughts(count):
+    """
+    Define the database of useful thoughts
+    """
+    S = session()
+    rval = [x[0] for x in
+            S.query(Thought.th_text).order_by(func.random()).limit(count)]
+    S.commit()
+    return rval
