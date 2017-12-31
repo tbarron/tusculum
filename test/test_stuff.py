a37fc5adb04713c534b5764e10764195ba1454f1
Tom Barron <tusculum@gmail.com>
2017-12-31 18:17:27 -0500

    Figure out how to access an iframe in selenium

--- test/test_stuff.py
+++ test/test_stuff.py
@@ -27,3 +27,24 @@ def web():
         driver.switch_to_window(driver.window_handles[0])
         time.sleep(2)
     driver.quit()
+# -----------------------------------------------------------------------------
+def test_find_iframe(web):
+    web.get("localhost:5000")
+    exp = {"Useful Thoughts": "x",
+           "source for tusculum.pythonanywhere.com": "x",
+           "Javascript, CSS, and HTML (JaCH) Doo-dads": "x",
+           "backscratcher": "x",
+           "evernote-helpers": "x",
+           "tbx - utility toolbox (Python)": "x"}
+    elm = web.find_elements_by_xpath("//iframe[@class='panel']")
+    elm = web.find_element_by_xpath("//iframe[@id='projects']")
+    web.switch_to_frame(elm)
+    for link in web.find_elements_by_xpath("//a"):
+        exp[link.text] += "w"
+
+    justweb = [x for x in exp.keys() if exp[x] == "w"]
+    justexp = [x for x in exp.keys() if exp[x] == "x"]
+    assert justweb == [], "In the webpage but not expected"
+    assert justexp == [], "Expected but not in the webpage"
+
+
