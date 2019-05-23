cbcc6c8984e5ea4ac5f67ae054211f5c8834527f
Tom Barron <tusculum@gmail.com>
2019-05-23 16:25:07 -0400

    Add comments to javascript code

--- static/js/degrees.js
+++ static/js/degrees.js
@@ -1,3 +1,19 @@
+// =============================================================================
+// This code is used by degrees.html. It provides conversions among
+// the various forms of GPS coordinates. The various forms are:
+//    d    fractional degrees                         ddd.ffff
+//    dm   int degree + fractional min                ddd mm.fff
+//    dms  int deg + int min + fractional sec         ddd mm ss.fff
+//
+// While writing this code, I learned that it's very helpful to do
+// <opt><command>I in Chrome when loading javascript since this will
+// show any syntax errors that will cause loading to fail and make the
+// code unrunnable.
+// =============================================================================
+
+// =============================================================================
+// If *str* represents a float value, raise an alert with *msg* then
+// return true. Otherwise, return false.
 function isaFloat(str, msg) {
     var fval = parseFloat(str);
     var ival = parseInt(fval);
@@ -9,6 +25,9 @@ function isaFloat(str, msg) {
     }
 }
 
+// =============================================================================
+// This function is fired by the first calculate button and computes
+// dm and dms values from d values.
 function calc_d() {
     var degrees_s = $("#d_deg").val();
     var degrees_f = parseFloat(degrees_s);
@@ -21,6 +40,9 @@ function calc_d() {
     $("#dms_sec").val(fd_to_dms_s(degrees_f));
 }
 
+// =============================================================================
+// This function is fired by the second calculate button and computes
+// d and dms values from dm values.
 function calc_dm() {
     var degrees_s = $("#dm_deg").val();
     var minutes_s = $("#dm_min").val();
@@ -44,6 +66,9 @@ function calc_dm() {
     $("#dms_sec").val(dm_to_dms_s(degrees_i, minutes_f));
 }
 
+// =============================================================================
+// This function is fired by the third calculate button and computes
+// d and dm values from dms values.
 function calc_dms() {
     var degrees_s = $("#dms_deg").val();
     var minutes_s = $("#dms_min").val();
@@ -66,10 +91,14 @@ function calc_dms() {
     $("#dm_min").val(dms_to_dm_m(degrees_i, minutes_i, seconds_f));
 }
 
+// =============================================================================
+// Given d value, return integer degrees
 function fd_to_dms_d(fdegrees) {
     return(parseInt(fdegrees));
 }
 
+// =============================================================================
+// Given d value, return integer minutes
 function fd_to_dms_m(fdegrees) {
     idegrees = parseInt(fdegrees);
     frac = fdegrees - idegrees;
@@ -77,6 +106,8 @@ function fd_to_dms_m(fdegrees) {
     return iminutes;
 }
 
+// =============================================================================
+// Given d value, return fractional seconds
 function fd_to_dms_s(fdegrees) {
     idegrees = parseInt(fdegrees);
     frac = fdegrees - idegrees;
@@ -86,44 +117,62 @@ function fd_to_dms_s(fdegrees) {
     return(secs);
 }
 
+// =============================================================================
+// Given d value, return integer degrees
 function fd_to_dm_d(fdegrees) {
     return fd_to_dms_d(fdegrees);
 }
 
+// =============================================================================
+// Given d value, return fractional minutes
 function fd_to_dm_m(fdegrees) {
     idegrees = parseInt(fdegrees);
     frac = fdegrees - idegrees;
     return(60 * frac);
 }
 
+// =============================================================================
+// Given dm value, return d value
 function dm_to_d_d(degrees, minutes) {
     return(degrees + (minutes / 60));
 }
 
+// =============================================================================
+// Given dm value, return integer degrees
 function dm_to_dms_d(degrees, minutes) {
     return(degrees);
 }
 
+// =============================================================================
+// Given dm value, return integer minutes
 function dm_to_dms_m(degrees, minutes) {
     return(parseInt(minutes));
 }
 
+// =============================================================================
+// Given dm value, return fractional seconds
 function dm_to_dms_s(degrees, minutes) {
     imins = parseInt(minutes);
     secs = (minutes - imins) * 60;
     return(secs)
 }
 
+// =============================================================================
+// Given dms value, return fractional degrees
 function dms_to_d_d(degrees, minutes, seconds) {
     sfrac = seconds / 60;
     result = degrees + (minutes + sfrac) / 60;
     return result;
 }
 
+// =============================================================================
+// Given dms value, return integer degrees
 function dms_to_dm_d(degrees, minutes, seconds) {
     return(degrees);
 }
 
+// =============================================================================
+// Given dms value, return fractional minutes
 function dms_to_dm_m(degrees, minutes, seconds) {
     sfrac = seconds / 60;
     return(minutes + sfrac);
