100a336e0bca0976bd90e0fee81a9b077bd34ada
Tom Barron <tusculum@gmail.com>
2019-05-29 09:37:19 -0400

    speed: Remove old degree-related stuff

--- static/js/speed.js
+++ static/js/speed.js
@@ -31,140 +31,38 @@ function fps_calc() {
     alert("This is fps_calc");
 }
 
-// =============================================================================
-// This function is fired by the second calculate button and computes
-// d and dms values from dm values.
-function calc_dm() {
-    var degrees_s = $("#dm_deg").val();
-    var minutes_s = $("#dm_min").val();
-
-    var degrees_f = parseFloat(degrees_s);
-    var degrees_i = parseInt(degrees_f);
-
-    if (isaFloat(degrees_s, "value " + degrees_s + " should be an int")) {
-        return;
-    }
-    var minutes_f = parseFloat(minutes_s);
-    if (60 <= minutes_f) {
-        alert("minutes must be < 60");
-        return;
-    }
-
-    $("#d_deg").val(dm_to_d_d(degrees_i, minutes_f));
-
-    $("#dms_deg").val(dm_to_dms_d(degrees_i, minutes_f));
-    $("#dms_min").val(dm_to_dms_m(degrees_i, minutes_f));
-    $("#dms_sec").val(dm_to_dms_s(degrees_i, minutes_f));
 }
 
 // =============================================================================
-// This function is fired by the third calculate button and computes
-// d and dm values from dms values.
-function calc_dms() {
-    var degrees_s = $("#dms_deg").val();
-    var minutes_s = $("#dms_min").val();
-    var seconds_s = $("#dms_sec").val();
-
-    var degrees_i = parseInt(degrees_s);
-    var minutes_i = parseInt(minutes_s);
-    var seconds_f = parseFloat(seconds_s);
-
-    if (isaFloat(degrees_s, "degrees may not be a float in this context")) {
-        return;
-    }
-    if (isaFloat(minutes_s, "minutes may not be a float in this context")) {
-        return;
-    }
-
-    $("#d_deg").val(dms_to_d_d(degrees_i, minutes_i, seconds_f));
-
-    $("#dm_deg").val(dms_to_dm_d(degrees_i, minutes_i, seconds_f));
-    $("#dm_min").val(dms_to_dm_m(degrees_i, minutes_i, seconds_f));
 }
 
 // =============================================================================
-// Given d value, return integer degrees
-function fd_to_dms_d(fdegrees) {
-    return(parseInt(fdegrees));
 }
 
 // =============================================================================
-// Given d value, return integer minutes
-function fd_to_dms_m(fdegrees) {
-    idegrees = parseInt(fdegrees);
-    frac = fdegrees - idegrees;
-    iminutes = parseInt(60 * frac);
-    return iminutes;
 }
 
 // =============================================================================
-// Given d value, return fractional seconds
-function fd_to_dms_s(fdegrees) {
-    idegrees = parseInt(fdegrees);
-    frac = fdegrees - idegrees;
-    fmins = 60 * frac;
-    imins = parseInt(fmins);
-    secs = 60 * (fmins - imins);
-    return(secs);
 }
 
 // =============================================================================
-// Given d value, return integer degrees
-function fd_to_dm_d(fdegrees) {
-    return fd_to_dms_d(fdegrees);
 }
 
 // =============================================================================
-// Given d value, return fractional minutes
-function fd_to_dm_m(fdegrees) {
-    idegrees = parseInt(fdegrees);
-    frac = fdegrees - idegrees;
-    return(60 * frac);
 }
 
 // =============================================================================
-// Given dm value, return d value
-function dm_to_d_d(degrees, minutes) {
-    return(degrees + (minutes / 60));
 }
 
 // =============================================================================
-// Given dm value, return integer degrees
-function dm_to_dms_d(degrees, minutes) {
-    return(degrees);
 }
 
 // =============================================================================
-// Given dm value, return integer minutes
-function dm_to_dms_m(degrees, minutes) {
-    return(parseInt(minutes));
 }
 
 // =============================================================================
-// Given dm value, return fractional seconds
-function dm_to_dms_s(degrees, minutes) {
-    imins = parseInt(minutes);
-    secs = (minutes - imins) * 60;
-    return(secs)
 }
 
 // =============================================================================
-// Given dms value, return fractional degrees
-function dms_to_d_d(degrees, minutes, seconds) {
-    sfrac = seconds / 60;
-    result = degrees + (minutes + sfrac) / 60;
-    return result;
 }
 
-// =============================================================================
-// Given dms value, return integer degrees
-function dms_to_dm_d(degrees, minutes, seconds) {
-    return(degrees);
-}
-
-// =============================================================================
-// Given dms value, return fractional minutes
-function dms_to_dm_m(degrees, minutes, seconds) {
-    sfrac = seconds / 60;
-    return(minutes + sfrac);
-}
