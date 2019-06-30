0b2434724d5999147728c63cda9b076604b9c870
Tom Barron <tusculum@gmail.com>
2019-06-30 13:35:09 -0400

    Notes about conversion factors

--- static/js/speed.js
+++ static/js/speed.js
@@ -10,26 +10,88 @@
 // furlong       0.001515     0.00497         1.0           4.97           8.0
 // kilometer     0.000305       0.001      0.2012            1.0         1.609
 // mile          0.000189    0.000621       0.125         0.6215           1.0
+//
+// mph -> mps
+//   mph * 0.44704 -> mps
+//     10 mph = 4.4704 mps                 TEST
+// fps -> mps
+//   fps * 0.3048 -> mps
+//     10 fps = 3.048 mps                 TEST
+// fps -> fpf
+//   fps * 1832.7273 -> fpf
+//     10 fps = 18327.273 fpf                 TEST
+// fps -> kph
+//   fps * 1.09728 -> kph
+//     10 fps = 10.9728 kph                 TEST
+// fps -> mph
+//   fps * 0.6818182 -> mph
+//     10 fps = 6.8182 mph                 TEST
+// mps -> fps
+//   mps * 3.28084 -> fps
+//     10 mps = 32.8084 fps                 TEST
+// mps -> fpf
+//   mps * 6012.89 -> fpf
+//     10 mps = 60128.9 fpf
+// mps -> kph
+//   mps * 3.6 -> kph
+//     10 mps = 36 kph                 TEST
+// mps -> mph
+//   mps * 2.236936 -> mph
+//     1 mps = 2.236936 mph                 TEST
+// fpf -> fps
+//   fpf * 0.00054563 -> fps
+//     10000 fpf = 5.4563 fps                 TEST
+// fpf -> mps
+//   fpf * 0.0001663095 -> mps
+//     10000 fpf = 1.663095 mps                 TEST
+// fpf -> kph
+//   fpf * 0.0005986 -> kph
+//     10000 fpf = 5.98714 kph                 TEST
+// fpf -> mph
+//   fpf * 0.0003720238 -> mph
+//     1 fpf = 0.0003720238 mph                 TEST
+// kph -> fps
+//   kph * 0.91134442 -> fps
+//     10 kph = 9.1134442 fps
+// kph -> mps
+//   kph * 0.2778 -> mps
+//     10 kph = 2.777778 mps
+// kph -> fpf
+//   1670.565 * kph -> fpf
+//     1 kph = 1670.25 fpf
+// kph -> mph
+//   0.6213712 * kph -> mph
+//     100 kph = 62.13712 mph
+// mph -> fps
+//   fps = 1.466667 * mph
+//     fph = 5280 * mph
+//     fps = fph / 3600 = 5280 * mph / 3600
+//     60 mph = 88 fps
+// mph -> mps
+//   mph * 0.44704 -> mps
+//     60 mph = 26.8224 mps
+// mph -> kph
+//   1.609344 * mph -> kph
+//     60 mph = 96.56 kph
+// mph -> fpf
+//   2688 * mph -> fpf
+//     10 miles / hour => 80 furlongs / hour
+//     In a fortnight, there are 24 * 14 = 336 hours
+//     Traveling at 10 mph will cover 80 * 336 = 26880 furlongs in a fortnight
+//     60 mph = 161280 fpf
 // =============================================================================
-
-// =============================================================================
-// If *str* represents a float value, raise an alert with *msg* then
-// return true. Otherwise, return false.
-function isaFloat(str, msg) {
-    var fval = parseFloat(str);
-    var ival = parseInt(fval);
-    if (fval - ival != 0) {
-        alert(msg);
-        return(true);
-    } else {
-        return(false);
-    }
-}
 
 // =============================================================================
 // Convert miles per hour to meters per second
 function mlph_mps(mlph) {
     mps = mlph * 1609.344 / 3600;
+    return(mps);
+}
+
+// =============================================================================
+// Convert miles per hour to meters per second
+function mlph_kph(mlph) {
+    mps = mlph * 1.609344;
     return(mps);
 }
 
